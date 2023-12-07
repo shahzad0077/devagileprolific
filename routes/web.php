cb89c6ed6f1fa3e671b3264db19570e2a452a72f
basil-89 <84768962+basil-89@users.noreply.github.com>
2023-12-07 18:24:22 +0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -239,6 +239,9 @@ Route::name('flags.')->namespace('App\Http\Controllers')->prefix('dashboard/flag
 Route::name('react.')->namespace('App\Http\Controllers')->prefix('dashboard/react')->group(function () {
     Route::get('{organizationid}', 'ReactController@index');
 });
+Route::name('linking.')->namespace('App\Http\Controllers')->prefix('dashboard/linking')->group(function () {
+    Route::get('{organizationid}/{type}', 'LinkingController@index');
+});
 //EpicComment
 Route::post('add-epic-comment', [App\Http\Controllers\ObjectiveController::class, 'SaveComment']);
 Route::post('update-epic-comment', [App\Http\Controllers\ObjectiveController::class, 'UpdateEpicComment']);
@@ -252,7 +255,7 @@ Route::post('add-new-quarter-value', [App\Http\Controllers\OrganizationControlle
 Route::post('update-new-quarter-value', [App\Http\Controllers\OrganizationController::class, 'UpdateQvalue']);
 Route::post('delete-new-quarter-value', [App\Http\Controllers\OrganizationController::class, 'DeleteQvalue']);
 Route::post('update-key-quarter-value', [App\Http\Controllers\OrganizationController::class, 'UpdateQkeyVal']);
-
+Route::post('add-key-quarter-value', [App\Http\Controllers\OrganizationController::class, 'AddnewKeyQvalue']);
 
 Route::get('get-team', [App\Http\Controllers\TeamController::class,'GetTeam']);
 Route::get('get-team-obj', [App\Http\Controllers\TeamController::class,'GetTeamObj']);
