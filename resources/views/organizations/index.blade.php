b5a73212e5a56a6ccfb2617ec0e35a53decdb227
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-12-07 18:02:52 +0500

    new changes

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -324,24 +324,198 @@ $var_objective = "Org";
                         </div>
                     </div>
                     <div class="progress-container">
-                        <div class="progress-bar">
-                          <div class="progress">
-                            <div class="progress-bar progress-bar-for-objective bg-success" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">70%</div>
-                          </div>
+                        <div class="custom-progress-bar progress-bar-green" style="width: 70%;"></div>
+                        <div class="custom-progress-bar progress-bar-blue" style="width: 50%;"></div>
+                        <div class="custom-progress-bar progress-bar-yellow" style="width: 30%;"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-green"></span>
+                                        <span class="objectivepointtittle">Done (30%)</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">12</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-green"></span>
+                                        <span class="objectivepointtittle">In Progress (30%)</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">30</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-blue"></span>
+                                        <span class="objectivepointtittle">To Do (30%)</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">40</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-blue"></span>
+                                        <span class="objectivepointtittle">Done</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">50</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-blue"></span>
+                                        <span class="objectivepointtittle">Risks</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">60</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-yellow"></span>
+                                        <span class="objectivepointtittle">Action</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">70</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-yellow"></span>
+                                        <span class="objectivepointtittle">Impediments</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">80</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="objectivepoints">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1 d-flex">
+                                        <span class="dotforobjectivedashboard progress-bar-yellow"></span>
+                                        <span class="objectivepointtittle">Blocker</span>
+                                   </div>
+                                    <div class="flex-grow-1 text-right">
+                                        <span class="objectivepointanswer">80</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
-                        <div class="progress-bar">
-                          <div class="progress">
-                            <div class="progress-bar progress-bar-for-objective bg-info" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
-                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-7">
+            <div class="col-md-6">
+                <div class="dashboard-card-third">
+                    <div class="dashboard-card-tittle-second row">
+                        <div class="col-md-10">
+                            <h4>Statistics</h4>
                         </div>
-
-                        <div class="progress-bar">
-                          <div class="progress">
-                            <div class="progress-bar progress-bar-for-objective bg-warning" role="progressbar" style="width: 30%;" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">30%</div>
-                          </div>
+                        <div class="col-md-2 text-right">
+                            <div class="dashboard-card-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img  src="{{url('public/assets/svg/more.svg')}}" width="20">
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="javascript:void(0)">Action One</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Action Two</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Action Three</a>
+                                </div>
+                            </div>
                         </div>
-                      </div>
+                        <div class="col-md-12">
+                            <div class="dashboard-card-subtittle">
+                                <p>All Key Results</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="chartforkeyresult">
+                                <canvas id="myChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="dashboard-card-third">
+                    <div class="dashboard-card-tittle-second row">
+                        <div class="col-md-10">
+                            <h4>Targets</h4>
+                        </div>
+                        <div class="col-md-2 text-right">
+                            <div class="dashboard-card-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img  src="{{url('public/assets/svg/more.svg')}}" width="20">
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="javascript:void(0)">Action One</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Action Two</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Action Three</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="dashboard-card-subtittle">
+                                <p>All Key Results</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <div class="circleprogressbar" role="circleprogressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:90"></div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                            <div class="targetprice">$124,301 <small>+3.5 %</small></div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                            <button class="seereportdashboard">See Report</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="dashboard-card-third">
+                    <div class="dashboard-card-tittle-second row">
+                        <div class="col-md-10">
+                            <h4>Targets</h4>
+                        </div>
+                        <div class="col-md-2 text-right">
+                            <div class="dashboard-card-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img  src="{{url('public/assets/svg/more.svg')}}" width="20">
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="javascript:void(0)">Action One</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Action Two</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Action Three</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="dashboard-card-subtittle">
+                                <p>All Key Results</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -354,12 +528,25 @@ $var_objective = "Org";
         <button class="btn btn-sm btn-dark" id="delete-button" onclick="delete_record();" type="button">Delete All</button>
     </div>
 </div> -->
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/easypiechart.min.js" integrity="sha512-1yldf7W5suy0ko2u4OGU1qyeGrzh9+A3uyWGH4ws8MbndaWxZsgnzy6uqqBq7NUU/ImI1Js5kqDbunovCN1JqA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 <script>
-  // Select the form element
-
+// Select the form element
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"],
+    datasets: [{
+      label: '',
+      data: [12, 19, 3, 17, 28, 24, 7, 17, 67, 47, 27, 87],
+      backgroundColor: "rgba(54,153,255,1)",
+      borderWidth: 1,
+      barThickness: 1
+    }]
+  }
+});
   function saveOrganization(){
 
     var file_data = $('#add_logo').prop('files')[0];
